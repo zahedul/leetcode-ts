61fe49f5fb555a7c738dcb2177cd5ca01cd8df64
Zahedul Alam <zahed@flarie.com>
2025-09-08 19:48:41 +0600

    Leetcode 448

---
+++ solutions/448-FindAllNumbersDisappearedInAnArray/FindAllNumbersDisappearedInAnArray.test.ts
@@ -0,0 +1,11 @@
+import {findDisappearedNumbers} from "./FindAllNumbersDisappearedInAnArray";
+
+describe('FindAllNumbersDisappearedInAnArray', () => {
+  it('Test 1', () => {
+    expect(findDisappearedNumbers([4,3,2,7,8,2,3,1])).toEqual([5, 6]);
+  });
+
+  it('Test 2', () => {
+    expect(findDisappearedNumbers([1, 1])).toEqual([2]);
+  });
+});
---
+++ solutions/448-FindAllNumbersDisappearedInAnArray/FindAllNumbersDisappearedInAnArray.ts
@@ -0,0 +1,17 @@
+export function findDisappearedNumbers(nums: number[]): number[] {
+  let result: number[] = [];
+
+  for (let i = 0; i < nums.length; i++) {
+    const index = Math.abs(nums[i]) - 1;
+
+    nums[index] = Math.abs(nums[index]) * -1;
+  }
+
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] > 0) {
+      result.push(i+1);
+    }
+  }
+
+  return result;
+}
