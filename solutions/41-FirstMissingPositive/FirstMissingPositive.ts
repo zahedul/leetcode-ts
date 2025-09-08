0e7e35a910394d829a2254f2f7d0f7360eb54641
Zahedul Alam <zahed@flarie.com>
2025-09-08 20:04:38 +0600

    Leetcode 41

---
+++ solutions/41-FirstMissingPositive/FirstMissingPositive.ts
@@ -0,0 +1,23 @@
+export function firstMissingPositive(nums: number[]): number {
+  let result: number = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    while (
+      nums[i] > 0 &&
+      nums[i] <= nums.length &&
+      nums[nums[i] - 1] !== nums[i]
+      ) {
+      const v = nums[i];
+      const dest = v - 1;
+      [nums[i], nums[dest]] = [nums[dest], nums[i]];
+    }
+  }
+
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] != i + 1) {
+      return i + 1;
+    }
+  }
+
+  return nums.length + 1;
+}
