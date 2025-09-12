1c65ba6438af509661a0736e1332015343a088fa
Zahedul Alam <zahed@flarie.com>
2025-09-12 19:22:32 +0600

    Leetcode 215

---
+++ solutions/215-KthLargestElementInAnArray/KthLargestElementInAnArray.test.ts
@@ -0,0 +1,11 @@
+import {findKthLargest} from "./KthLargestElementInAnArray";
+
+describe('KthLargestElementInAnArray', () => {
+  it('Test 1', async () => {
+    expect(findKthLargest([3,2,1,5,6,4], 2)).toEqual(5);
+  });
+
+  it('Test 2', async () => {
+    expect(findKthLargest([3,2,3,1,2,4,5,5,6], 4)).toEqual(4);
+  });
+});
---
+++ solutions/215-KthLargestElementInAnArray/KthLargestElementInAnArray.ts
@@ -0,0 +1,76 @@
+class MinHeap {
+  private readonly heap: number[];
+  constructor() {
+    this.heap = [];
+  }
+
+  private swap(i: number, j: number) {
+    [this.heap[i], this.heap[j]] = [this.heap[j], this.heap[i]];
+  }
+
+  push(val: number) {
+    this.heap.push(val);
+    this.bubbleUp();
+  }
+
+  pop() {
+    if (this.heap.length === 0) return undefined;
+    if (this.heap.length === 1) return this.heap.pop();
+
+    const top = this.heap[0];
+    this.heap[0] = this.heap.pop()!;
+    this.bubbleDown();
+    return top;
+  }
+
+  private bubbleUp() {
+    let idx = this.heap.length - 1;
+    while (idx > 0) {
+      const parent = Math.floor((idx - 1) / 2);
+      if (this.heap[parent] <= this.heap[idx]) break;
+      this.swap(parent, idx);
+      idx = parent;
+    }
+  }
+
+  private bubbleDown() {
+    let idx = 0;
+    const length = this.heap.length;
+    while (true) {
+      const left = 2 * idx + 1;
+      const right = 2 * idx + 2;
+      let smallest = idx;
+
+      if (left < length && this.heap[left] < this.heap[smallest]) {
+        smallest = left;
+      }
+      if (right < length && this.heap[right] < this.heap[smallest]) {
+        smallest = right;
+      }
+      if (smallest === idx) break;
+
+      this.swap(idx, smallest);
+      idx = smallest;
+    }
+  }
+
+  size() {
+    return this.heap.length;
+  }
+
+  peek() {
+    return this.heap[0];
+  }
+}
+
+export function findKthLargest(nums: number[], k: number): number {
+  let minHeap = new MinHeap();
+  for (let num of nums) {
+    minHeap.push(num);
+    if (minHeap.size() > k) {
+      minHeap.pop();
+    }
+  }
+
+  return minHeap.peek()!;
+}
