ebffd0349d473f59c6081f85ed861c37054e85db
Zahedul Alam <z.alam015@gmail.com>
2025-10-03 11:39:53 +0600

    310

---
+++ solutions/310-MinimumHeightTrees/MinimumHeightTrees.test.ts
@@ -0,0 +1,11 @@
+import {findMinHeightTrees} from "./MinimumHeightTrees";
+
+describe('MinimumHeightTrees', () => {
+  it('Test 1', () => {
+    expect(findMinHeightTrees(4, [[1, 0],[1, 2],[1, 3]])).toEqual([1]);
+  });
+
+  it('Test 2', () => {
+    expect(findMinHeightTrees(6, [[3, 0],[3, 1],[3, 2],[3, 4],[5, 4]])).toEqual([3, 4]);
+  });
+});
---
+++ solutions/310-MinimumHeightTrees/MinimumHeightTrees.ts
@@ -0,0 +1,43 @@
+export function findMinHeightTrees(n: number, edges: number[][]): number[] {
+  if (n === 1) {
+    return [0];
+  }
+
+  const adj: number[][] = Array.from({ length: n }, () => []);
+
+  for (const [n1, n2] of edges) {
+    adj[n1].push(n2);
+    adj[n2].push(n1);
+  }
+
+  const edgeCnt: number[] = Array(n).fill(0);
+  const leaves: number[] = [];
+
+  for (let i = 0; i < n; i++) {
+    edgeCnt[i] = adj[i].length;
+    if (adj[i].length === 1) {
+      leaves.push(i);
+    }
+  }
+
+  while (leaves.length !== 0) {
+    if ( n <= 2) {
+      return leaves;
+    }
+
+    const size = leaves.length;
+
+    for (let i: number = 0; i < size; i++) {
+      const node: number = leaves.shift()!;
+      n--;
+      for (const nei of adj[node]) {
+        edgeCnt[nei]--;
+        if (edgeCnt[nei] === 1) {
+          leaves.push(nei);
+        }
+      }
+    }
+  }
+
+  return [];
+}
